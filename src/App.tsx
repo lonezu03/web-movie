107da42c3e4db8914072d97948abe2e1ed28cd34
lonezu03 <118800029+lonezu03@users.noreply.github.com>
2024-04-23 16:41:34 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { Component, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import From from './component/from.tsx'
-import TrangChu from './component/trangchinh.tsx'
-import Phim from'./component/phim.tsx'
-import { Link, Route, Routes } from 'react-router-dom'
-import Home from './component/hometong.tsx' 
-import Xemphim from './component/xemphim.tsx'
+//import { Component, useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import "./App.css";
+import From from "./component/from.tsx";
+import TrangChu from "./component/trangchinh.tsx";
+//import Phim from'./component/phim.tsx'
+import { Route, Routes } from "react-router-dom";
+import Home from "./component/hometong.tsx";
+import Xemphim from "./component/xemphim.tsx";
 function App() {
- return <div id='app'>
-    <Routes>
-    <Route path='/xemphim' element={<Xemphim/>}/>
+  return (
+    <div id="app">
+      <Routes>
+        <Route path="/xemphim" element={<Xemphim />} />
 
-    <Route path='/trangchu' element={<TrangChu/>}/>
-    <Route path='/dk' element={<From/>}/>
-    <Route path='/' element={<Home/>}/>
-    
-</Routes> 
-
- </div>
+        <Route path="/trangchu" element={<TrangChu />} />
+        <Route path="/dk" element={<From />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
