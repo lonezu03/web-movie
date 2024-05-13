4831f0a361ad7cbc837adfb0a9aacda80f2e3970
lonezu03 <118800029+lonezu03@users.noreply.github.com>
2024-05-13 23:22:00 +0700

    11:21

--- src/component/phimnoibat.tsx
+++ src/component/phimnoibat.tsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faCircleChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
 
 const Phimnoibat = () => {
   const dsphim = [
@@ -22,38 +26,94 @@ const Phimnoibat = () => {
       mota: "Để cứu lấy chúng sinh, con gái tông chưởng môn quay ngược về quá khứ để ngăn cản tên ma thần trước khi hắn bị tà cốt trong người xâm chiếm hoàn toàn.",
     },
   ];
-  //const [phim, setPhim] = useState(dsphim[0]);
-  //console.log(phim)
+  const dsphimngan = [
+    {
+      id: 98,
+      name: "Bầu trời rực đỏ",
+      link_img:
+        "https://images.fpt.shop/unsafe/filters:quality(5)/fptshop.com.vn/uploads/images/tin-tuc/176627/Originals/poster-phim-hoat-hinh-1.jpg",
 
-  const phim = {
-    id: 99,
-    name: "Trường nguyệt tẫn minh",
-    link_img:
-      "https://images.fpt.shop/unsafe/filters:quality(5)/fptshop.com.vn/uploads/images/tin-tuc/176627/Originals/poster-phim-hoat-hinh-1.jpg",
+      link: "https://trailer.vieon.vn/Teaser_BauTroiRucDo.mp4",
+      mota: "Quỷ Vương trùng sinh vào Ha Ram. Nữ thần sinh mệnh lấy đi đôi mắt của cậu để nhường người sẽ giải cứu thế giới. Thiên mệnh 2 đứa trẻ bắt đầu.",
+    },
+    {
+      
 
-    link: "https://www.youtube.com/embed/5XPaB6Gu9XY",
-    mota: "Để cứu lấy chúng sinh, con gái tông chưởng môn quay ngược về quá khứ để ngăn cản tên ma thần trước khi hắn bị tà cốt trong người xâm chiếm hoàn toàn.",
-  };
+      id: 99,
+      name: "Trường nguyệt tẫn minh",
+      link_img:
+        "https://images.fpt.shop/unsafe/filters:quality(5)/fptshop.com.vn/uploads/images/tin-tuc/176627/Originals/poster-phim-hoat-hinh-1.jpg",
 
+      link: "https://trailer.vieon.vn/Teaser_TruongNguyetTanMinhv3.mp4",
+      mota: "Để cứu lấy chúng sinh, con gái tông chưởng môn quay ngược về quá khứ để ngăn cản tên ma thần trước khi hắn bị tà cốt trong người xâm chiếm hoàn toàn.",
+    }
+   
+  ];
+  const phim = dsphim[0];
+  const [phimngan, setPhimngan] = useState(dsphimngan[0]);
+
+  //console.log(phim);
+function next()
+{
+  
+  let tam
+ for (let i = 0;i<dsphimngan.length;i++)
+  {
+    if(dsphimngan[i].id==phimngan.id+1)
+      tam=dsphimngan[i];
+  }
+  console.log(tam)
+
+  if(tam!=null)
+{
+
+  setPhimngan(tam);
+  console.log('da set')
+} 
+  else
+    setPhimngan(dsphimngan[0])
+  
+}
+function back()
+{   let tam
+  for (let i = 0;i<dsphimngan.length;i++)
+   {
+     if(dsphimngan[i].id==phimngan.id-1)
+       tam=dsphimngan[i];
+   }
+   console.log(tam)
+ 
+   if(tam!=null)
+     setPhimngan(tam);
+   else
+     setPhimngan(dsphimngan[dsphimngan.length])
+
+}
   return (
     <div className="phimnoibat">
       <div>
         <div id="noibat">
-          <video width="1500px" autoPlay muted loop id="vd">
+          <video width="1500px" autoPlay muted  id="vd">
             <source
-              src="https://trailer.vieon.vn/Teaser_TruongNguyetTanMinhv3.mp4"
+              src={phimngan.link}
               type="video/mp4"
             />
+            
           </video>
 
           <div className="noidungpnb">
-            <p id="tenphim">{phim.name}</p>
-            <Link to={`/xemphim/${phim.id}`} state={{ phim }} id="link">
+            <p id="tenphim">{phimngan.name}</p>
+            <Link to={`/xemphim/${phimngan.id}`} state={{ phim }} id="link">
               Watch now
             </Link>
-            <p>{phim.mota}</p>
+            <p>{phimngan.mota}</p>
           </div>
-        </div>
+          <div id="iconnext">
+          <FontAwesomeIcon icon={faCircleChevronLeft} size="3x" onClick={back}/>
+          <FontAwesomeIcon icon={faCircleChevronRight}size="3x" onClick={next}/>
+          
+    </div>
+            </div>
       </div>
 
       <div></div>
